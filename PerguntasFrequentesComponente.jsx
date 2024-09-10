cd9eb2a865fd10012ed32c06bc0b2f30a148dc1b
Antony-Marcos <142536686+Antony-Marcos@users.noreply.github.com>
2024-09-10 16:05:35 -0300

    Add files via upload

---
+++ PerguntasFrequentesComponente.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+function PerguntasFrequentes() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  // Array de objetos com perguntas e respostas
+  const faqData = [
+    {
+      question: "Como o sistema de gestão de atendimento pode beneficiar minha empresa?",
+      answer: "O sistema pode aumentar a eficiência, melhorar a comunicação e fornecer relatórios detalhados para uma gestão mais eficaz."
+    },
+    {
+      question: "Posso personalizar o sistema de acordo com as necessidades da minha empresa?",
+      answer: "Sim, o sistema é altamente personalizável para atender às necessidades específicas de cada empresa."
+    },
+    {
+      question: "Como posso acompanhar o desempenho da minha equipe?",
+      answer: "Você pode usar o sistema para gerar relatórios de desempenho e acompanhar métricas importantes em tempo real."
+    },
+    {
+      question: "O sistema é seguro?",
+      answer: "Sim, implementamos as melhores práticas de segurança para proteger seus dados e garantir a confidencialidade."
+    },
+    {
+      question: "Posso integrar o sistema com outras ferramentas que minha empresa já utiliza?",
+      answer: "Sim, nosso sistema suporta integração com várias ferramentas e aplicativos existentes."
+    },
+    {
+      question: "Outros",
+      answer: "Se você tiver outras perguntas, entre em contato com nosso suporte para obter mais informações."
+    }
+  ];
+
+  return (
+    <div>
+      <NavBar />
+      <div className="h-screen flex flex-col items-center justify-center px-4 py-8 bg-left">
+        <h1 className="font-bold text-black text-5xl mb-8 text-center">Perguntas Frequentes</h1>
+        <div className="flex flex-col items-center gap-6 w-full max-w-[600px]">
+          {faqData.map((item, index) => (
+            <div key={index} className="w-full">
+              <button 
+                onClick={() => handleToggle(index)}
+                className="bg-white p-4 w-full flex items-center justify-between font-bold text-lg text-black rounded-lg border-4 border-gray-900 duration-300">
+                {item.question}
+              </button>
+              {openIndex === index && (
+                <div className="mt-2 p-4 border-t-2 border-gray-300 w-full text-center">
+                  <p className="text-lg text-gray-700">{item.answer}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default PerguntasFrequentes;
---
+++ routes.jsx
@@ -0,0 +1,97 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/home';
+import CadastroColaborador from './components/CadastroColaborador/cadastroColaborador';
+import App from './App';
+import FuncionarioHomePage from './pages/funcionarioHomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import CustomerHome from './pages/CustomerHome';
+import AdminHome from './pages/AdminHome';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import DepartmentTickets from './pages/DepartmentTickets';
+import CentralDeAjudaPage from './pages/CentralDeAjudaPage'
+import HomeAdmPage from './pages/HomeAdm';
+import CreateTicket from './pages/CreateTicket';
+import PerguntasFrequentes from './pages/PerguntasFrequentes';
+
+
+function Main() {
+
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <Home />
+        } />
+        <Route path="/teste" element={<CentralDeAjudaPage/>}/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/homeAdm" element={<HomeAdmPage />} />
+        <Route
+          path="/cadastroColaborador"
+          element={
+            <ProtectedRoute allowedTypes={['ADMIN']}>
+              <CadastroColaborador />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/app" element={<App />} />
+        <Route
+          path="/funcionario"
+          element={
+            <ProtectedRoute allowedTypes={['EMPLOYEE']}>
+              <FuncionarioHomePage />
+            </ProtectedRoute>
+          }
+        />
+       
+        {/* TODO: turn into a private route for EMPLOYEE */}
+        <Route 
+          path='department-tickets' 
+          element={
+            <ProtectedRoute allowedTypes={['EMPLOYEE']}>
+              <DepartmentTickets />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/customerHome"
+          element={
+            <ProtectedRoute allowedTypes={['CUSTOMER']}>
+              <CentralDeAjudaPage/>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/adminHome"
+          element={
+            <ProtectedRoute allowedTypes={['ADMIN']}>
+              <AdminHome />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+        path="/Create-Ticket"
+        element={
+          <ProtectedRoute allowedTypes={['CUSTOMER']}>
+            <CreateTicket/>
+          </ProtectedRoute>
+        
+        }
+        />
+        <Route
+        path="/Perguntas-Frequentes"
+        element={
+          
+            <PerguntasFrequentes/>
+          
+        
+        }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Main;
